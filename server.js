fba7a81dfbb32dfc72b904c2f79a35712b9128d4
Nguyen Thuy Lien <73010204+LienNguyen2912@users.noreply.github.com>
2024-01-28 22:16:16 +0900

    Update server.js

--- server.js
+++ server.js
@@ -8,13 +8,20 @@ app.use(express.json());
 app.post('/delayed', (req, res) => {
   // Simulate a delay of 2 seconds before responding
   setTimeout(() => {
-    res.json({ message: 'hello world after delaying 5s!' });
+    res.json({ message: 'OMG! Hello world after delaying 5s!' });
   }, 5000);
 });
 
+// Route for POST method with very long delay
+app.post('/verydelayed', (req, res) => {
+  // Simulate a delay of 5 minutes before responding
+  setTimeout(() => {
+    res.json({ message: 'Thanks for waiting. Hello world after delaying 5 minutes!' });
+  }, 300000);
+});
 // Route for GET method
 app.get('/', (req, res) => {
-  res.json({ message: 'Server is up and running babeeee**!' });
+  res.json({ message: '**Server is up and running my babe**!' });
 });
 
 app.listen(port, () => {
